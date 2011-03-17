cb8ffe856bc8a12d90e0dcf378ce637e227010e2
Jeremy Kelley <jeremy@collectivelabs.com>
2011-03-17 15:56:11 -0500

    trailing whitespace ZAP

--- mogo/field.py
+++ mogo/field.py
@@ -1,15 +1,15 @@
 import inspect
 
 class Field(object):
-    """ 
+    """
     This class may eventually do type-checking, default values,
     etc. but for right now it's for subclassing and glorified
     documentation.
     """
-    
+
     default = None
     value_type = None
-    
+
     def __init__(self, value_type=None, default=None):
         if default != None:
             raise NotImplementedError(
@@ -18,9 +18,9 @@ class Field(object):
         if value_type == None:
             value_type = str
         self.value_type = value_type
-        
+
 class ReferenceField(Field):
     """ Simply holds information about the reference model. """
-    
+
     def __init__(self, model):
         self.model = model
